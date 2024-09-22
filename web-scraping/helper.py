ce0f4c7daa7df12e059cee86359c4e9693f0b62a
cyjico <going2email2@gmail.com>
2024-09-22 14:53:46 +0800

    Add scraping

--- web-scraping/helper.py
+++ web-scraping/helper.py
@@ -1,5 +1,9 @@
 import random
 import requests
+import re
+import bs4
+
+__all__ = ["get_request", "get_route_coords"]
 
 user_agents = [
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36",
@@ -9,7 +13,46 @@ user_agents = [
 ]
 
 
-def get_request(url):
+def get_request(url: str) -> requests.Response:
     headers = {"user-agent": random.choice(user_agents)}
 
     return requests.get(url, headers=headers)
+
+
+def get_route_coords(url: str) -> list[tuple[str, ...]]:
+    if (res := get_request(url)).status_code == 200:
+        soup = bs4.BeautifulSoup(res.text, "html.parser")
+
+        if (entry_content := soup.select_one("article > div > div")) is not None:
+            assert isinstance(entry_content, bs4.PageElement)
+
+            iframe_all = entry_content.find_all_next("iframe")
+            iframe_index = 0
+
+            if len(iframe_all) > 1:
+                while not (
+                    iframe_all[iframe_index]
+                    .get("src")
+                    .startswith("https://www.google.com")
+                ):
+                    iframe_index += 1
+
+            if (iframe := iframe_all[iframe_index]) is not None:
+                assert isinstance(iframe, bs4.PageElement)
+
+                if (res := get_request(iframe.get("src"))).status_code == 200:
+                    soup = bs4.BeautifulSoup(res.text, "html.parser")
+
+                    if (script := soup.find("script")) is not None:
+                        assert isinstance(script, bs4.PageElement)
+
+                        return re.findall(
+                            r"(\[\[([\d.-]+),([\d.-]+)\]\],?)+",
+                            re.findall(r'(?<=_pageData = ").*(?=")', script.text)[0],
+                        )
+                else:
+                    raise Exception(res)
+    else:
+        raise Exception(res)
+
+    return []
--- web-scraping/index.py
+++ web-scraping/index.py
@@ -1,28 +1,7 @@
-import re
-from bs4 import BeautifulSoup, Tag
-from helper import get_request
+from helper import *
 
-if (
-    res := get_request(
-        "https://ph.commutetour.com/ph/routes/davao-routes/davao-jeep/route-2-davao-city-jeep/"
+print(
+    get_route_coords(
+        "https://ph.commutetour.com/ph/routes/davao-routes/davao-jeep/route-1-davao-city-jeep/"
     )
-).status_code == 200:
-    soup = BeautifulSoup(res.text, "html.parser")
-
-    if (div := soup.find("div", attrs={"id": "route"})) is not None:
-        if (iframe := div.find("iframe")) is not None:
-            assert isinstance(iframe, Tag)
-            res = get_request(iframe.get("src"))
-
-            if res.status_code == 200:
-                soup = BeautifulSoup(res.text, "html.parser")
-
-                script = soup.find("script")
-                assert isinstance(script, Tag)
-
-                # TODO: Extract coordinates from _pageData variable.
-                print(re.findall(r'(?<=_pageData = ").*(?=")', script.text)[0])
-            else:
-                print(res)
-else:
-    print(res)
+)
