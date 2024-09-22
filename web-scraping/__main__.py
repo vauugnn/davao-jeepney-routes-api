74370b2bab3010e96572c9864edce6f7b2232d00
cyjico <going2email2@gmail.com>
2024-09-22 15:42:49 +0800

    Add looping through all routes

--- web-scraping/__main__.py
+++ web-scraping/__main__.py
@@ -1,7 +1,17 @@
 from src.util import *
 
-print(
-    get_route_coords(
-        "https://ph.commutetour.com/ph/routes/davao-routes/davao-jeep/route-1-davao-city-jeep/"
-    )
-)
+# Run python3 web-scraping or python web-scraping from the root directory
+for i in range(0, 14):
+    for letter in ["", "a", "b"]:
+        url = (
+            "https://ph.commutetour.com/ph/routes/davao-routes/davao-jeep/route-%s-davao-city-jeep/"
+            % (str(i + 1) + letter)
+        )
+
+        try:
+            coords = get_route_coords(url)
+
+            if len(coords) > 1:
+                print(url)
+        except BaseException as e:
+            print("%s from %s" % (e, url))
